8f5671897bdedb1835f12e6fd6e449989c4ffadc
Mohammed Uthmaan Poese​​ <31779778+mupoese@users.noreply.github.com>
2025-02-16 14:47:35 +0100

    Update ocr.py

--- app/ocr.py
+++ app/ocr.py
@@ -1,1 +1,9 @@
+import fitz  # PyMuPDF
+import pytesseract
+from pdf2image import convert_from_path
 
+def pdf_to_images(pdf_path):
+    return convert_from_path(pdf_path)
+
+def ocr_image(image):
+    return pytesseract.image_to_string(image)
