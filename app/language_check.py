b86b7ef3a10b51d8b2cfab362bd806980f547408
Mohammed Uthmaan Poese​​ <31779778+mupoese@users.noreply.github.com>
2025-02-16 14:47:58 +0100

    Update language_check.py

--- app/language_check.py
+++ app/language_check.py
@@ -1,1 +1,10 @@
+from langdetect import detect
 
+def detect_language(text):
+    return detect(text)
+
+def word_by_word_check(text):
+    words = text.split()
+    for word in words:
+        # Voeg hier taalvalidatie of correctie toe
+        pass
