5b90b0c4cf5b01bd0acef25ad4885912e934e2f9
Mohammed Uthmaan Poese​​ <31779778+mupoese@users.noreply.github.com>
2025-02-16 14:48:21 +0100

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,10 @@
+import os
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+def check_folders(input_folder, output_folder):
+    if not os.path.exists(input_folder):
+        os.makedirs(input_folder)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
